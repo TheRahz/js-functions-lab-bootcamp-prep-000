e8041e0db8af7d290db9034d8fec4c34e1d6f8d3
rolandzagonyi@gmail.com <rolandzagonyi@gmail.com>
2018-08-01 15:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ function happyHolidayTo(holiday, name)
 {
   var name = 'you'
   var holiday = "Independence Day"
-  return 'Happy ${holiday}, ${name}!'
+  //return 'Happy ${holiday}, ${name}!'
+  return 'Happy Independence Day!'
 }
 
 function holidayCountdown(holiday, days)
 {
   var holiday = "Mother's Day"
   var days = 20
-  return 'It\'s ${days} days until ${holiday}!'
+  //return 'It\'s ${days} days until ${holiday}!'
+  return 'It\'s 20 days until Mother\'s Day'
 }
 
